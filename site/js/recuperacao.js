fbf49e23dd9801bf4195753e7919c4e3c4c90066
brunolimabh <bruno.lima@spctech.school>
2023-04-07 13:19:48 -0300

    rec senha

--- site/js/recuperacao.js
+++ site/js/recuperacao.js
@@ -1,8 +1,10 @@
-
-
 function enviar() {
     div_tela1.style.display = "none";
     div_tela2.style.display = "block";
+}
+function cancelar() {
+    div_tela1.style.display = "flex";
+    div_tela2.style.display = "none";
 }
 function continuar() {
     div_tela2.style.display = "none";
