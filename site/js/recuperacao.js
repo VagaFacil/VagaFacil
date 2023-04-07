2d4ddbdd282cf0755b45b6153687ca1588c3711f
gabrielBmoraes <gabriel.moraes@sptech.school>
2023-04-07 12:50:44 -0300

    atualização de recuperação de senha

---
+++ site/js/recuperacao.js
@@ -0,0 +1,12 @@
+function enviar() {
+    div_tela1.style.display = "none";
+    div_tela2.style.display = "block";
+}
+function cancelar() {
+    div_tela1.style.display = "flex";
+    div_tela2.style.display = "none";
+}
+function continuar() {
+    div_tela2.style.display = "none";
+    div_tela3.style.display = "block";
+}
