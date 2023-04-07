cd9c2080c67314b47103e8d21a531c3652401a1c
brunolimabh <bruno.lima@spctech.school>
2023-04-06 22:39:47 -0300

    Login

--- site/js/login.js
+++ site/js/login.js
@@ -1,13 +1,28 @@
 
+
+
+
+
+
 function logar() {
     var email = ipt_email.value;
     var senha = ipt_senha.value;
-    var vaga = 'vaga';
-    var facil = 'facil'
+    var vaga = 'vaga@gmail.com';
+    var facil = 'facil';
+    var erro = false;
+
+    if (email.indexOf('@') < 0 && email.indexOf('.com') < 0) {
+        alert('Insira um email válido')
+        erro = true;
+    } else if (email != vaga) {
+        alert('Email não cadastrado')
+        erro = true
+    } else if (senha != facil) {
+        alert('Senha incorreta')
+        erro = true;
+    }
     
-    if (email == vaga && senha == facil) {
+    if (erro == false && email == vaga && senha == facil) {
         location.href = '../daniel/dashboard.html';
-    } else {
-        alert('Email ou senha incorreto!')
     }
 }
