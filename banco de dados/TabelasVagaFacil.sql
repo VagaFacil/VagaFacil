417543341df04f5e9fe8c1b9ad078149231cf666
lucasrocha2704 <lucas.rocha@sptech.school>
2023-04-11 22:23:51 -0300

    Atualização Tabela VagaFácil

--- banco de dados/TabelasVagaFacil.sql
+++ banco de dados/TabelasVagaFacil.sql
@@ -37,7 +37,291 @@ create table sensor(
 create table dados(
 		idDados int primary key auto_increment,
 		dataHora datetime,
-		valor varchar(45),
+		valor char(1),
 		fkSensor int,
 		foreign key(fksensor) references sensor(idsensor)
 )auto_increment=200000;
+
+insert into sensor values (null,'04100-000', '202', '1');
+insert into sensor values (null,'03100-000', '307', '2');
+insert into sensor values (null,'03000-000', '472', '3');
+insert into sensor values (null,'04500-000', '532', '4');
+insert into sensor values (null,'04700-000', '298', '5');
+insert into sensor values (null,'05300-000', '397', '6');
+insert into sensor values (null,'05400-000', '485', '7');
+insert into sensor values (null,'05800-000', '604', '8');
+insert into sensor values (null,'01000-000', '200', '9');
+insert into sensor values (null,'01300-000', '578', '10');
+
+select * from sensor;
+
+-- Dados referenter au sensor Numero 1
+insert into dados values (null, '2023-04-10 07:00:00', '0', 150000);
+insert into dados values (null, '2023-04-10 07:30:00', '0', 150000);
+insert into dados values (null, '2023-04-10 08:00:00', '0', 150000);
+insert into dados values (null, '2023-04-10 08:30:00', '1', 150000);
+insert into dados values (null, '2023-04-10 09:00:00', '1', 150000);
+insert into dados values (null, '2023-04-10 09:30:00', '1', 150000);
+insert into dados values (null, '2023-04-10 10:00:00', '1', 150000);
+insert into dados values (null, '2023-04-10 10:30:00', '1', 150000);
+insert into dados values (null, '2023-04-10 11:00:00', '0', 150000);
+insert into dados values (null, '2023-04-10 11:30:00', '0', 150000);
+insert into dados values (null, '2023-04-10 12:00:00', '1', 150000);
+insert into dados values (null, '2023-04-10 12:30:00', '1', 150000);
+insert into dados values (null, '2023-04-10 13:00:00', '1', 150000);
+insert into dados values (null, '2023-04-10 13:30:00', '1', 150000);
+insert into dados values (null, '2023-04-10 14:00:00', '1', 150000);
+insert into dados values (null, '2023-04-10 14:30:00', '1', 150000);
+insert into dados values (null, '2023-04-10 15:00:00', '1', 150000);
+insert into dados values (null, '2023-04-10 15:30:00', '0', 150000);
+insert into dados values (null, '2023-04-10 16:00:00', '0', 150000);
+insert into dados values (null, '2023-04-10 16:30:00', '0', 150000);
+insert into dados values (null, '2023-04-10 17:00:00', '0', 150000);
+insert into dados values (null, '2023-04-10 17:30:00', '1', 150000);
+insert into dados values (null, '2023-04-10 18:00:00', '1', 150000);
+insert into dados values (null, '2023-04-10 18:30:00', '1', 150000);
+insert into dados values (null, '2023-04-10 19:00:00', '1', 150000);
+
+-- Dados referenter au sensor Numero 2
+insert into dados values (null, '2023-04-10 07:00:00', '1', 150001);
+insert into dados values (null, '2023-04-10 07:30:00', '1', 150001);
+insert into dados values (null, '2023-04-10 08:00:00', '0', 150001);
+insert into dados values (null, '2023-04-10 08:30:00', '0', 150001);
+insert into dados values (null, '2023-04-10 09:00:00', '0', 150001);
+insert into dados values (null, '2023-04-10 09:30:00', '1', 150001);
+insert into dados values (null, '2023-04-10 10:00:00', '1', 150001);
+insert into dados values (null, '2023-04-10 10:30:00', '1', 150001);
+insert into dados values (null, '2023-04-10 11:00:00', '0', 150001);
+insert into dados values (null, '2023-04-10 11:30:00', '1', 150001);
+insert into dados values (null, '2023-04-10 12:00:00', '1', 150001);
+insert into dados values (null, '2023-04-10 12:30:00', '1', 150001);
+insert into dados values (null, '2023-04-10 13:00:00', '1', 150001);
+insert into dados values (null, '2023-04-10 13:30:00', '1', 150001);
+insert into dados values (null, '2023-04-10 14:00:00', '1', 150001);
+insert into dados values (null, '2023-04-10 14:30:00', '1', 150001);
+insert into dados values (null, '2023-04-10 15:00:00', '1', 150001);
+insert into dados values (null, '2023-04-10 15:30:00', '1', 150001);
+insert into dados values (null, '2023-04-10 16:00:00', '0', 150001);
+insert into dados values (null, '2023-04-10 16:30:00', '0', 150001);
+insert into dados values (null, '2023-04-10 17:00:00', '0', 150001);
+insert into dados values (null, '2023-04-10 17:30:00', '1', 150001);
+insert into dados values (null, '2023-04-10 18:00:00', '1', 150001);
+insert into dados values (null, '2023-04-10 18:30:00', '1', 150001);
+insert into dados values (null, '2023-04-10 19:00:00', '0', 150001);
+
+-- Dados referenter au sensor Numero 3
+insert into dados values (null, '2023-04-10 07:00:00', '1', 150002);
+insert into dados values (null, '2023-04-10 07:30:00', '1', 150002);
+insert into dados values (null, '2023-04-10 08:00:00', '0', 150002);
+insert into dados values (null, '2023-04-10 08:30:00', '1', 150002);
+insert into dados values (null, '2023-04-10 09:00:00', '0', 150002);
+insert into dados values (null, '2023-04-10 09:30:00', '1', 150002);
+insert into dados values (null, '2023-04-10 10:00:00', '1', 150002);
+insert into dados values (null, '2023-04-10 10:30:00', '1', 150002);
+insert into dados values (null, '2023-04-10 11:00:00', '0', 150002);
+insert into dados values (null, '2023-04-10 11:30:00', '1', 150002);
+insert into dados values (null, '2023-04-10 12:00:00', '1', 150002);
+insert into dados values (null, '2023-04-10 12:30:00', '1', 150002);
+insert into dados values (null, '2023-04-10 13:00:00', '0', 150002);
+insert into dados values (null, '2023-04-10 13:30:00', '1', 150002);
+insert into dados values (null, '2023-04-10 14:00:00', '1', 150002);
+insert into dados values (null, '2023-04-10 14:30:00', '1', 150002);
+insert into dados values (null, '2023-04-10 15:00:00', '0', 150002);
+insert into dados values (null, '2023-04-10 15:30:00', '1', 150002);
+insert into dados values (null, '2023-04-10 16:00:00', '0', 150002);
+insert into dados values (null, '2023-04-10 16:30:00', '0', 150002);
+insert into dados values (null, '2023-04-10 17:00:00', '1', 150002);
+insert into dados values (null, '2023-04-10 17:30:00', '1', 150002);
+insert into dados values (null, '2023-04-10 18:00:00', '1', 150002);
+insert into dados values (null, '2023-04-10 18:30:00', '1', 150002);
+insert into dados values (null, '2023-04-10 19:00:00', '0', 150002);
+
+-- Dados referenter au sensor Numero 4
+insert into dados values (null, '2023-04-10 07:00:00', '1', 150003);
+insert into dados values (null, '2023-04-10 07:30:00', '1', 150003);
+insert into dados values (null, '2023-04-10 08:00:00', '0', 150003);
+insert into dados values (null, '2023-04-10 08:30:00', '1', 150003);
+insert into dados values (null, '2023-04-10 09:00:00', '0', 150003);
+insert into dados values (null, '2023-04-10 09:30:00', '0', 150003);
+insert into dados values (null, '2023-04-10 10:00:00', '1', 150003);
+insert into dados values (null, '2023-04-10 10:30:00', '1', 150003);
+insert into dados values (null, '2023-04-10 11:00:00', '0', 150003);
+insert into dados values (null, '2023-04-10 11:30:00', '1', 150003);
+insert into dados values (null, '2023-04-10 12:00:00', '0', 150003);
+insert into dados values (null, '2023-04-10 12:30:00', '1', 150003);
+insert into dados values (null, '2023-04-10 13:00:00', '0', 150003);
+insert into dados values (null, '2023-04-10 13:30:00', '1', 150003);
+insert into dados values (null, '2023-04-10 14:00:00', '1', 150003);
+insert into dados values (null, '2023-04-10 14:30:00', '0', 150003);
+insert into dados values (null, '2023-04-10 15:00:00', '0', 150003);
+insert into dados values (null, '2023-04-10 15:30:00', '1', 150003);
+insert into dados values (null, '2023-04-10 16:00:00', '0', 150003);
+insert into dados values (null, '2023-04-10 16:30:00', '0', 150003);
+insert into dados values (null, '2023-04-10 17:00:00', '1', 150003);
+insert into dados values (null, '2023-04-10 17:30:00', '1', 150003);
+insert into dados values (null, '2023-04-10 18:00:00', '0', 150003);
+insert into dados values (null, '2023-04-10 18:30:00', '1', 150003);
+insert into dados values (null, '2023-04-10 19:00:00', '1', 150003);
+
+-- Dados referenter au sensor Numero 5
+insert into dados values (null, '2023-04-10 07:00:00', '1', 150004);
+insert into dados values (null, '2023-04-10 07:30:00', '1', 150004);
+insert into dados values (null, '2023-04-10 08:00:00', '1', 150004);
+insert into dados values (null, '2023-04-10 08:30:00', '1', 150004);
+insert into dados values (null, '2023-04-10 09:00:00', '1', 150004);
+insert into dados values (null, '2023-04-10 09:30:00', '0', 150004);
+insert into dados values (null, '2023-04-10 10:00:00', '1', 150004);
+insert into dados values (null, '2023-04-10 10:30:00', '1', 150004);
+insert into dados values (null, '2023-04-10 11:00:00', '0', 150004);
+insert into dados values (null, '2023-04-10 11:30:00', '1', 150004);
+insert into dados values (null, '2023-04-10 12:00:00', '0', 150004);
+insert into dados values (null, '2023-04-10 12:30:00', '1', 150004);
+insert into dados values (null, '2023-04-10 13:00:00', '0', 150004);
+insert into dados values (null, '2023-04-10 13:30:00', '1', 150004);
+insert into dados values (null, '2023-04-10 14:00:00', '1', 150004);
+insert into dados values (null, '2023-04-10 14:30:00', '0', 150004);
+insert into dados values (null, '2023-04-10 15:00:00', '0', 150004);
+insert into dados values (null, '2023-04-10 15:30:00', '1', 150004);
+insert into dados values (null, '2023-04-10 16:00:00', '0', 150004);
+insert into dados values (null, '2023-04-10 16:30:00', '1', 150004);
+insert into dados values (null, '2023-04-10 17:00:00', '1', 150004);
+insert into dados values (null, '2023-04-10 17:30:00', '1', 150004);
+insert into dados values (null, '2023-04-10 18:00:00', '1', 150004);
+insert into dados values (null, '2023-04-10 18:30:00', '1', 150004);
+insert into dados values (null, '2023-04-10 19:00:00', '1', 150004);
+
+-- Dados referenter au sensor Numero 5
+insert into dados values (null, '2023-04-10 07:00:00', '1', 150005);
+insert into dados values (null, '2023-04-10 07:30:00', '1', 150005);
+insert into dados values (null, '2023-04-10 08:00:00', '1', 150005);
+insert into dados values (null, '2023-04-10 08:30:00', '1', 150005);
+insert into dados values (null, '2023-04-10 09:00:00', '1', 150005);
+insert into dados values (null, '2023-04-10 09:30:00', '0', 150005);
+insert into dados values (null, '2023-04-10 10:00:00', '1', 150005);
+insert into dados values (null, '2023-04-10 10:30:00', '1', 150005);
+insert into dados values (null, '2023-04-10 11:00:00', '0', 150005);
+insert into dados values (null, '2023-04-10 11:30:00', '1', 150005);
+insert into dados values (null, '2023-04-10 12:00:00', '0', 150005);
+insert into dados values (null, '2023-04-10 12:30:00', '1', 150005);
+insert into dados values (null, '2023-04-10 13:00:00', '0', 150005);
+insert into dados values (null, '2023-04-10 13:30:00', '1', 150005);
+insert into dados values (null, '2023-04-10 14:00:00', '1', 150005);
+insert into dados values (null, '2023-04-10 14:30:00', '0', 150005);
+insert into dados values (null, '2023-04-10 15:00:00', '0', 150005);
+insert into dados values (null, '2023-04-10 15:30:00', '1', 150005);
+insert into dados values (null, '2023-04-10 16:00:00', '0', 150005);
+insert into dados values (null, '2023-04-10 16:30:00', '1', 150005);
+insert into dados values (null, '2023-04-10 17:00:00', '1', 150005);
+insert into dados values (null, '2023-04-10 17:30:00', '1', 150005);
+insert into dados values (null, '2023-04-10 18:00:00', '1', 150005);
+insert into dados values (null, '2023-04-10 18:30:00', '1', 150005);
+insert into dados values (null, '2023-04-10 19:00:00', '1', 150005);
+
+-- Dados referenter au sensor Numero 7
+insert into dados values (null, '2023-04-10 07:00:00', '0', 150006);
+insert into dados values (null, '2023-04-10 07:30:00', '1', 150006);
+insert into dados values (null, '2023-04-10 08:00:00', '1', 150006);
+insert into dados values (null, '2023-04-10 08:30:00', '0', 150006);
+insert into dados values (null, '2023-04-10 09:00:00', '1', 150006);
+insert into dados values (null, '2023-04-10 09:30:00', '0', 150006);
+insert into dados values (null, '2023-04-10 10:00:00', '1', 150006);
+insert into dados values (null, '2023-04-10 10:30:00', '1', 150006);
+insert into dados values (null, '2023-04-10 11:00:00', '0', 150006);
+insert into dados values (null, '2023-04-10 11:30:00', '1', 150006);
+insert into dados values (null, '2023-04-10 12:00:00', '1', 150006);
+insert into dados values (null, '2023-04-10 12:30:00', '1', 150006);
+insert into dados values (null, '2023-04-10 13:00:00', '0', 150006);
+insert into dados values (null, '2023-04-10 13:30:00', '0', 150006);
+insert into dados values (null, '2023-04-10 14:00:00', '1', 150006);
+insert into dados values (null, '2023-04-10 14:30:00', '0', 150006);
+insert into dados values (null, '2023-04-10 15:00:00', '0', 150006);
+insert into dados values (null, '2023-04-10 15:30:00', '1', 150006);
+insert into dados values (null, '2023-04-10 16:00:00', '0', 150006);
+insert into dados values (null, '2023-04-10 16:30:00', '1', 150006);
+insert into dados values (null, '2023-04-10 17:00:00', '1', 150006);
+insert into dados values (null, '2023-04-10 17:30:00', '0', 150006);
+insert into dados values (null, '2023-04-10 18:00:00', '0', 150006);
+insert into dados values (null, '2023-04-10 18:30:00', '1', 150006);
+insert into dados values (null, '2023-04-10 19:00:00', '1', 150006);
+
+-- Dados referenter au sensor Numero 8
+insert into dados values (null, '2023-04-10 07:00:00', '0', 150007);
+insert into dados values (null, '2023-04-10 07:30:00', '0', 150007);
+insert into dados values (null, '2023-04-10 08:00:00', '1', 150007);
+insert into dados values (null, '2023-04-10 08:30:00', '0', 150007);
+insert into dados values (null, '2023-04-10 09:00:00', '1', 150007);
+insert into dados values (null, '2023-04-10 09:30:00', '0', 150007);
+insert into dados values (null, '2023-04-10 10:00:00', '1', 150007);
+insert into dados values (null, '2023-04-10 10:30:00', '1', 150007);
+insert into dados values (null, '2023-04-10 11:00:00', '0', 150007);
+insert into dados values (null, '2023-04-10 11:30:00', '1', 150007);
+insert into dados values (null, '2023-04-10 12:00:00', '1', 150007);
+insert into dados values (null, '2023-04-10 12:30:00', '1', 150007);
+insert into dados values (null, '2023-04-10 13:00:00', '1', 150007);
+insert into dados values (null, '2023-04-10 13:30:00', '0', 150007);
+insert into dados values (null, '2023-04-10 14:00:00', '1', 150007);
+insert into dados values (null, '2023-04-10 14:30:00', '0', 150007);
+insert into dados values (null, '2023-04-10 15:00:00', '0', 150007);
+insert into dados values (null, '2023-04-10 15:30:00', '0', 150007);
+insert into dados values (null, '2023-04-10 16:00:00', '1', 150007);
+insert into dados values (null, '2023-04-10 16:30:00', '1', 150007);
+insert into dados values (null, '2023-04-10 17:00:00', '1', 150007);
+insert into dados values (null, '2023-04-10 17:30:00', '0', 150007);
+insert into dados values (null, '2023-04-10 18:00:00', '0', 150007);
+insert into dados values (null, '2023-04-10 18:30:00', '1', 150007);
+insert into dados values (null, '2023-04-10 19:00:00', '1', 150007);
+
+-- Dados referenter au sensor Numero 9
+insert into dados values (null, '2023-04-10 07:00:00', '1', 150008);
+insert into dados values (null, '2023-04-10 07:30:00', '0', 150008);
+insert into dados values (null, '2023-04-10 08:00:00', '1', 150008);
+insert into dados values (null, '2023-04-10 08:30:00', '0', 150008);
+insert into dados values (null, '2023-04-10 09:00:00', '0', 150008);
+insert into dados values (null, '2023-04-10 09:30:00', '0', 150008);
+insert into dados values (null, '2023-04-10 10:00:00', '1', 150008);
+insert into dados values (null, '2023-04-10 10:30:00', '1', 150008);
+insert into dados values (null, '2023-04-10 11:00:00', '1', 150008);
+insert into dados values (null, '2023-04-10 11:30:00', '1', 150008);
+insert into dados values (null, '2023-04-10 12:00:00', '1', 150008);
+insert into dados values (null, '2023-04-10 12:30:00', '1', 150008);
+insert into dados values (null, '2023-04-10 13:00:00', '0', 150008);
+insert into dados values (null, '2023-04-10 13:30:00', '0', 150008);
+insert into dados values (null, '2023-04-10 14:00:00', '1', 150008);
+insert into dados values (null, '2023-04-10 14:30:00', '0', 150008);
+insert into dados values (null, '2023-04-10 15:00:00', '0', 150008);
+insert into dados values (null, '2023-04-10 15:30:00', '1', 150008);
+insert into dados values (null, '2023-04-10 16:00:00', '1', 150008);
+insert into dados values (null, '2023-04-10 16:30:00', '1', 150008);
+insert into dados values (null, '2023-04-10 17:00:00', '1', 150008);
+insert into dados values (null, '2023-04-10 17:30:00', '0', 150008);
+insert into dados values (null, '2023-04-10 18:00:00', '0', 150008);
+insert into dados values (null, '2023-04-10 18:30:00', '1', 150008);
+insert into dados values (null, '2023-04-10 19:00:00', '1', 150008);
+
+-- Dados referenter au sensor Numero 10
+insert into dados values (null, '2023-04-10 07:00:00', '1', 150009);
+insert into dados values (null, '2023-04-10 07:30:00', '0', 150009);
+insert into dados values (null, '2023-04-10 08:00:00', '1', 150009);
+insert into dados values (null, '2023-04-10 08:30:00', '0', 150009);
+insert into dados values (null, '2023-04-10 09:00:00', '0', 150009);
+insert into dados values (null, '2023-04-10 09:30:00', '0', 150009);
+insert into dados values (null, '2023-04-10 10:00:00', '1', 150009);
+insert into dados values (null, '2023-04-10 10:30:00', '1', 150009);
+insert into dados values (null, '2023-04-10 11:00:00', '0', 150009);
+insert into dados values (null, '2023-04-10 11:30:00', '0', 150009);
+insert into dados values (null, '2023-04-10 12:00:00', '1', 150009);
+insert into dados values (null, '2023-04-10 12:30:00', '1', 150009);
+insert into dados values (null, '2023-04-10 13:00:00', '0', 150009);
+insert into dados values (null, '2023-04-10 13:30:00', '0', 150009);
+insert into dados values (null, '2023-04-10 14:00:00', '1', 150009);
+insert into dados values (null, '2023-04-10 14:30:00', '0', 150009);
+insert into dados values (null, '2023-04-10 15:00:00', '1', 150009);
+insert into dados values (null, '2023-04-10 15:30:00', '1', 150009);
+insert into dados values (null, '2023-04-10 16:00:00', '1', 150009);
+insert into dados values (null, '2023-04-10 16:30:00', '1', 150009);
+insert into dados values (null, '2023-04-10 17:00:00', '1', 150009);
+insert into dados values (null, '2023-04-10 17:30:00', '1', 150009);
+insert into dados values (null, '2023-04-10 18:00:00', '0', 150009);
+insert into dados values (null, '2023-04-10 18:30:00', '1', 150009);
+insert into dados values (null, '2023-04-10 19:00:00', '0', 150009);
+select * from dados where fkSensor=150009;
