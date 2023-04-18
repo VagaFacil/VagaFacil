40b065443175c0164839ae3ad2e6bf666113e7aa
brunolimabh <bruno.lima@spctech.school>
2023-04-18 10:43:16 -0300

    Adicionar usuario

--- site/dashboard/js/menuH.js
+++ site/dashboard/js/menuH.js
@@ -6,5 +6,17 @@ function menuH() {
 
 function seta() {
     listaMenuH.style.display = 'none';
+}
 
+function sair() {
+    alertSair.style.display = 'block';
+}
+
+function sim() {
+    location.href = '../index.html'
+    alertSair.style.display = 'none';
+}
+
+function nao() {
+    alertSair.style.display = 'none';
 }
