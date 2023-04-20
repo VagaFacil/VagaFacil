8eda09d2ab42ff8d453bcd30d31c2d628934b02c
lucasrocha2704 <lucas.rocha@sptech.school>
2023-04-19 21:57:07 -0300

    Inicio tela Perfil

---
+++ site/dashboard/js/perfil.js
@@ -0,0 +1,27 @@
+function exibir_informacaoPessoal() {
+    pessoal.style = "display: flex;";
+    comercial.style = "display: none;";
+    atuacao.style = "display: none;";
+    personalizar.style = "display: none;";
+}
+
+function exibir_comercial() {
+    pessoal.style = "display: none;";
+    comercial.style = "display: flex;";
+    atuacao.style = "display: none;";
+    personalizar.style = "display: none;";
+}
+
+function exibir_atuacao() {
+    pessoal.style = "display: none;";
+    comercial.style = "display: none;";
+    atuacao.style = "display: flex;";
+    personalizar.style = "display: none;";
+}
+
+function exibir_personalizar() {
+    pessoal.style = "display: none;";
+    comercial.style = "display: none;";
+    atuacao.style = "display: none;";
+    personalizar.style = "display: flex;";
+}
