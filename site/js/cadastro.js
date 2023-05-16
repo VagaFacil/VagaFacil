f4a603762c7f5ac4b559d66a8dece845b0ce31ed
brunolimabh <bruno.lima@spctech.school>
2023-05-16 09:08:24 -0300

    API web-data-viz

--- site/js/cadastro.js
+++ site/js/cadastro.js
@@ -1,37 +1,6 @@
-// Variáveis globais
 
-// card1
-var erroNome = false;
-var erroCargo = false;
-var erroDatan = false;
-var erroCpf = false;
-var erroTelefone = false;
-
-// card2
-var erroRazao = false;
-var erroCnpj = false;
-var erroCep = false;
-var erroEndereco = false;
-var erroNumero = false;
-var erroComp = false;
-var erroBairro = false;
-
-// card3
-var erroEmail = false;
-var erroSenha = false;
-var erroSenhaRep = false;
-
-// cards geral
-var validarCard1 = false;
-var validarCard2 = false;
-var validarCard3 = false;
-
-
-// Validações
 
 function cadastrar() {
-    // card1
-    // nome
     if (ipt_nome.value.length < 3) {
         vNome.style.display = 'block';
         ipt_nome.style = 'border-color: red';
@@ -41,7 +10,6 @@ function cadastrar() {
         ipt_nome.style = 'border-color: #32a7b1';
         erroNome = false;
     }
-    // sobrenome
     if (ipt_cargo.value.length < 3) {
         vCargo.style.display = 'block';
         ipt_cargo.style = 'border-color: red';
@@ -51,14 +19,8 @@ function cadastrar() {
         ipt_cargo.style = 'border-color: #32a7b1';
         erroCargo = false;
     }
-    // data de nascimento
-    // cpf
-    // telefone
-    // tem erro no card?
     validarCard1 = erroNome == false && erroCargo == false && erroDatan == false && erroCpf == false && erroTelefone == false;
     
-    // card2
-    // razão
     if (ipt_razao.value.length < 3) {
         vNome.style.display = 'block';
         ipt_nome.style = 'border-color: red';
@@ -68,17 +30,7 @@ function cadastrar() {
         ipt_nome.style = 'border-color: #32a7b1';
         erroNome = false;
     }
-    // cnpj
-    // cep
-    // endereço
-    // número
-    // complemento - sem validação
-    // bairro
-    // tem erro no card?
     validarCard2 = erroRazao == false && erroCnpj == false && erroCep == false && erroEndereco == false && erroNumero == false && erroComp == false && erroBairro == false;
-
-    // card3
-    // email
     if (ipt_email.value.indexOf('@') < 0 && ipt_email.value.indexOf('.com') < 0) {
         vEmail.style.display = 'block';
         ipt_email.style = 'border-color: red';
@@ -88,12 +40,7 @@ function cadastrar() {
         ipt_email.style = 'border-color: #32a7b1';
         erroEmail = false;
     }
-    // senha
-    // senha repetida
-    // tem erro no card?
     validarCard3 = erroEmail == false && erroSenha == false && erroSenhaRep == false;
-
-    // validar os 3 cards para seguir
     if (validarCard1 && validarCard2 && validarCard3) {
         location.href = '../login.html'
     }
