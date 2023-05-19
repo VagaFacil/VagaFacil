765bfa4f566522dcd2578866c2a7936e678f7845
brunolimabh <bruno.lima@spctech.school>
2023-05-19 12:37:20 -0300

    Validação login

--- web-data-viz/site/public/js/funcoes.js
+++ web-data-viz/site/public/js/funcoes.js
@@ -37,11 +37,11 @@ function finalizarAguardar(texto) {
     var divAguardar = document.getElementById("div_aguardar");
     divAguardar.style.display = "none";
 
-    var divErrosLogin = document.getElementById("div_erros_login");
-    if (texto) {
-        divErrosLogin.style.display = "flex";
-        divErrosLogin.innerHTML = texto;
-    }
+    // var divErrosLogin = document.getElementById("div_erros_login");
+    // if (texto) {
+    //     divErrosLogin.style.display = "flex";
+    //     divErrosLogin.innerHTML = texto;
+    // }
 }
 
 
