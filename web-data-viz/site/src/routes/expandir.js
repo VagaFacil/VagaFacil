d3fd464593732b8fe31f047128264739c6ed94db
HenriqueBechis <Henrique.coelho@sptech.school>
2023-05-24 09:06:47 -0300

    Gŕafico acompanhar

---
+++ web-data-viz/site/src/routes/expandir.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var router = express.Router();
+
+var expandirController = require("../controllers/expandirController");
+
+router.get("/", function (req, res) {
+    expandirController.testar(req, res);
+});
+
+router.get("/listarBairro", function (req, res) {
+    expandirController.listarBairro(req, res);
+});
+
+//Recebendo os dados do html e direcionando para a função cadastrar de expandirController.js
+/* router.post("/cadastrar", function (req, res) {
+    expandirController.cadastrar(req, res);
+})
+
+router.post("/autenticar", function (req, res) {
+    expandirController.entrar(req, res);
+}); */
+
+module.exports = router;
